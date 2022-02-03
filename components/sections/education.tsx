e50d193eb019fa3109be2c05d82db871f8c5dde6
Pablo J Torrubiano <me@pablotorrubiano.xyz>
2022-02-03 18:48:05 +0100

    Added education screen

---
+++ components/sections/education.tsx
@@ -0,0 +1,39 @@
+import { LegacyRef } from 'react';
+import { useTranslation } from 'next-i18next';
+
+import classNames from '../../utils/tailwindClassNamesHelper';
+import Position from '../position';
+import Section from './sectionContainer';
+
+import styles from '../../styles/tailwindStyles.json';
+
+const studies = [
+  'fiuba',
+  'cbc',
+  'enspa',
+  'fuce',
+];
+
+const sections = [
+  {
+    id: 'description',
+    contentType: 'single',
+    showTitle: false,
+  },
+];
+
+const Education = ({ innerRef }: { innerRef?: LegacyRef<HTMLDivElement>; }) => {
+  const { t } = useTranslation(['common', 'education']);
+  return (
+    <Section id="education" innerRef={innerRef}>
+      <h2 className={classNames(styles.text['section-title'], styles.text['gradient-b'])}>
+        {t('SECTIONS.EDUCATION')}
+      </h2>
+      {studies.map((study) => (
+        <Position i18nKey={study} namespace='education' key={study} sections={sections} highlight='a'/>
+      ))}
+    </Section>
+  );
+};
+
+export default Education;
