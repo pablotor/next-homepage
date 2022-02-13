69a4fa8574c854a4128f829a4411c872ef79f6eb
Pablo J Torrubiano <me@pablotorrubiano.xyz>
2022-02-14 00:48:11 +0100

    Updated portfolio section with table

--- components/sections/portfolio.tsx
+++ components/sections/portfolio.tsx
@@ -2,9 +2,79 @@ import { LegacyRef } from 'react';
 import { useTranslation } from 'next-i18next';
 
 import classNames from '../../utils/tailwindClassNamesHelper';
+import PortfolioTable from '../portfolioTable';
 import Section from './sectionContainer';
 
 import styles from '../../styles/tailwindStyles.json';
+
+const portfolio = [
+  {
+    id: 'homesite',
+    type: 'frontend',
+    lastUpdated: '2020-01-07',
+    openSource: true,
+    techs: [
+      {
+        id: 'tailwind',
+        src: '/img/tailwind.svg',
+        style: 'bg-[#1C5C78] p-1',
+      }, {
+        id: 'react',
+        src: '/img/react.svg',
+        style: 'bg-[#17333B] p-1',
+      }, {
+        id: 'next',
+        src: '/img/next.svg',
+      }, {
+        id: 'ts',
+        src: '/img/typescript.svg',
+        style: 'bg-[#3178C6] pb-0.5',
+      },
+    ],
+  }, {
+    id: 'wibson',
+    type: 'fullstack',
+    lastUpdated: '2020-01-07',
+    openSource: false,
+    techs: [],
+  }, {
+    id: 'linbot',
+    type: 'backend',
+    lastUpdated: '2020-01-07',
+    openSource: true,
+    techs: [
+      {
+        id: 'puppeteer',
+        src: '/img/puppeteer.svg',
+        style: 'bg-[#17333B] p-1',
+      }, {
+        id: 'node',
+        src: '/img/node.svg',
+        style: 'bg-white',
+      }, {
+        id: 'ts',
+        src: '/img/typescript.svg',
+        style: 'bg-[#3178C6] pb-0.5',
+      },
+    ],
+  }, {
+    id: 'appointer',
+    type: 'backend',
+    lastUpdated: '2020-01-07',
+    openSource: true,
+    techs: [
+      {
+        id: 'node',
+        src: '/img/node.svg',
+        style: 'bg-white',
+      }, {
+        id: 'js',
+        src: '/img/javascript.svg',
+        style: 'bg-[#f7df1e] pb-0.5',
+      },
+    ],
+  },
+];
 
 const Portfolio = ({ innerRef }: { innerRef?: LegacyRef<HTMLDivElement>; }) => {
   const { t } = useTranslation(['common', 'portfolio']);
@@ -16,6 +86,7 @@ const Portfolio = ({ innerRef }: { innerRef?: LegacyRef<HTMLDivElement>; }) => {
       <p className={styles.text.secondary}>
         {t('COMMENT', { ns: 'portfolio' })}
       </p>
+      <PortfolioTable portfolio={portfolio} namespace='portfolio' />
     </Section>
   );
 };
