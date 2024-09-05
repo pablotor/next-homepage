dc9ca0b967e42139425fe3ab79ebc10d4bc6c68a
pablotor <me@pablotor.dev>
2024-09-05 10:52:21 +0200

    Code improved

--- app/[lng]/components/languageSwitcher.tsx
+++ app/[lng]/components/languageSwitcher.tsx
@@ -4,6 +4,12 @@ import type { WithLanguage } from '../../i18n';
 
 import classNames from '../utils/classNames';
 
+const getStyle = (isSelected: boolean) => (
+  isSelected
+    ? 'group-hover:text-gray-900 text-transparent gradient-a'
+    : 'group-hover:text-transparent gradient-b'
+);
+
 type LanguageSwitcherProps = {
   className?: string;
 } & WithLanguage;
@@ -12,18 +18,15 @@ const LanguageSwitcher: FC<LanguageSwitcherProps> = ({ lng, className }) => (
   <a
     href={lng === 'es' ? '/en' : '/es'}
     className={classNames(
-      'title flex p-3 text-gray-900',
-      lng === 'es'
-        ? '[&_span:last-child]:hover:text-transparent [&_span:first-child]:text-transparent [&_span:first-child]:hover:text-gray-900'
-        : '[&_span:first-child]:hover:text-transparent [&_span:last-child]:text-transparent [&_span:last-child]:hover:text-gray-900',
+      'title flex p-3 text-gray-900 group',
       className,
     )}
   >
-    <span className={classNames('bg-clip-text transition-all', lng === 'en' ? 'gradient-a' : 'gradient-b')}>
+    <span className={classNames('bg-clip-text transition-all', getStyle(lng === 'en'))}>
       EN
     </span>
-    <span>{' / '}</span>
-    <span className={classNames('bg-clip-text transition-all', lng === 'es' ? 'gradient-a' : 'gradient-b')}>
+    <span className="mx-1">/</span>
+    <span className={classNames('bg-clip-text transition-all', getStyle(lng === 'es'))}>
       ES
     </span>
   </a>
