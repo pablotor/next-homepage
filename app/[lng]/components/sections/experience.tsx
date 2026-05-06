ad762ff05e5977082be15bf5e15595bc629d9235
pablotor <me@pablotor.dev>
2026-05-06 11:10:26 +0300

    feat: position component and text improvements

--- app/[lng]/components/sections/experience.tsx
+++ app/[lng]/components/sections/experience.tsx
@@ -3,47 +3,43 @@ import Link from 'next/link';
 
 import type { WithLanguage } from '../../../i18n';
 
-import { useTranslation } from '../../../i18n';
-import Position, { SectionData } from '../position';
-
-const jobs = ['wye', 'wibson', 'first_data', 'conexia'];
-
-const sections: SectionData[] = [
-  {
-    id: 'responsibilities',
-    contentType: 'list',
-    showTitle: true,
-  },
-  {
-    id: 'projects',
-    contentType: 'list',
-    showTitle: true,
-  },
-];
+import { getServerSideTranslations } from '../../../i18n';
+import Position, { PositionItem } from '../position';
 
 const Experience: FC<WithLanguage> = async ({ lng }) => {
-  // eslint-disable-next-line react-hooks/rules-of-hooks
-  const { t } = await useTranslation(lng, ['experience', 'common']);
+  const { t } = await getServerSideTranslations(lng, ['experience', 'common']);
+  const experienceItems = t('ITEMS', {
+    returnObjects: true,
+    ns: 'experience',
+  }) as PositionItem[];
+  const experienceSections = t('SECTIONS', {
+    returnObjects: true,
+    ns: 'experience',
+  }) as { KEY: keyof PositionItem; LABEL: string }[];
   return (
     <>
       <h2 className="section-title gradient-a">
         {t('SECTIONS.EXPERIENCE', { ns: 'common' })}
       </h2>
-      <p className="subtitle">{t('COMMENT')}</p>
+      <p className="subtitle">{t('COMMENT', { ns: 'experience' })}</p>
       <div>
-        {jobs.map((job) => (
+        {experienceItems.map((item) => (
           <Position
-            i18nKey={job}
-            namespace="experience"
-            key={job}
-            sections={sections}
+            key={item.TITLE}
+            position={item}
+            sections={experienceSections}
             includeSecondary
-            lng={lng}
             interpolationComponents={{
               ai: (
                 <Link
                   href="https://illow.io"
                   className="gradient-b bg-clip-text transition-colors hover:text-transparent"
+                />
+              ),
+              ad: (
+                <Link
+                  href="https://www.digital-shores.com"
+                  className="gradient-b  bg-clip-text transition-all hover:text-transparent"
                 />
               ),
             }}
